1eabc059e6f89bf9f13965759aed73e9519ae5f3
Justin Linn <justinslinn727@gmail.com>
2021-07-14 12:31:43 -0400

    Extract vars into functions

--- app.js
+++ app.js
@@ -5,68 +5,71 @@ const options = { cellDates: true };
 const workbook = xlsx.readFile('VISN-17_Facility_HS_before.xlsx', options);
 const worksheet = workbook.Sheets['Sheet1'];
 
-// turn worksheet data into JSON so we can modify the data
-const JSONData = xlsx.utils.sheet_to_json(worksheet);
+// TODO: put helps in another file
 
-// remove rows that include N/A
-const removeNA = JSONData.filter((record) => !record['ID'].includes('n/a'));
-// remove rows that include "COVID-19 Vaccines"
-const JSONDataWithoutCOVID = removeNA.filter((record) => !record['ID'].includes('COVID-19'));
+// helpers
+const removeNonApplicable = (JSONData) => JSONData.filter((record) => !record['ID'].includes('n/a'));
+const removeCOVIDRecords = (JSONData) => JSONData.filter((record) => !record['ID'].includes('COVID-19'));
+const restructureRows = (JSONData) => {
+  return JSONData.map((record) => {
+    /* Data shape of record
+      {
+       "ID": "vha_740GC - Pharmacy",
+       "Facility ID": "vha_740GC",
+       "Facility": "Corpus Christi VA Clinic",
+       "VACM System Health Service": "Pharmacy at VA Texas Valley health care",
+       "Health Services": "Pharmacy",
+       "Health System": "VA Texas Valley health care",
+       "Owner": "VA Texas Valley health care"
+    }*/
 
-const removeAndAddRows = JSONDataWithoutCOVID.map((record) => {
-  /* Data shape of record
-    {
-     "ID": "vha_740GC - Pharmacy",
-     "Facility ID": "vha_740GC",
-     "Facility": "Corpus Christi VA Clinic",
-     "VACM System Health Service": "Pharmacy at VA Texas Valley health care",
-     "Health Services": "Pharmacy",
-     "Health System": "VA Texas Valley health care",
-     "Owner": "VA Texas Valley health care"
-  }*/
+    // delete column B = Facility ID
+    delete record['Facility ID'];
+    // delete column E = Health Services
+    delete record['Health Services'];
+    // delete column F = Health system
+    delete record['Health System'];
+    // keep columns ID, facility, VAMC System Health Service, Facility description, owner
 
-  // delete column B = Facility ID
-  delete record['Facility ID'];
-  // delete column E = Health Services
-  delete record['Health Services'];
-  // delete column F = Health system
-  delete record['Health System'];
-  // keep columns ID, facility, VAMC System Health Service, Facility description, owner
+    // if there is not a Facility description add a blank one
+    const hasFacilityDescription = !!record['Facility description of services'];
+    hasFacilityDescription ? record['Facility description of services'] : (record['Facility description of services'] = '');
 
-  // if there is not a Facility description add a blank one
-  const hasFacilityDescription = !!record['Facility description of services'];
-  hasFacilityDescription ? record['Facility description of services'] : (record['Facility description of services'] = '');
+    return record;
+  });
+};
 
-  return record;
-});
-
-const removeDuplicateRecords = removeAndAddRows.reduce((acc, record) => {
-  const hasDuplicate = (row) => {
-    return row.ID === record.ID;
-  };
-
-  if (!acc.some(hasDuplicate)) {
-    acc.push(record);
-  }
-
-  return acc;
-}, []);
-
-const escapeAposFromRecords = removeDuplicateRecords.map((record) => {
-  const recordKeys = Object.keys(record);
-
-  const newData = recordKeys.reduce((acc, key) => {
-    const value = record[key];
-    const cleanedValue = value.replace(/'/g, '"');
-
-    return {
-      ...acc,
-      [key]: cleanedValue,
+const removeDuplicateRecords = (JSONData) => {
+  return JSONData.reduce((acc, record) => {
+    const hasDuplicate = (row) => {
+      return row.ID === record.ID;
     };
-  }, {});
 
-  return newData;
-});
+    if (!acc.some(hasDuplicate)) {
+      acc.push(record);
+    }
+
+    return acc;
+  }, []);
+};
+
+const escapeAposFromRecords = (JSONData) => {
+  return JSONData.map((record) => {
+    const recordKeys = Object.keys(record);
+
+    const newData = recordKeys.reduce((acc, key) => {
+      const value = record[key];
+      const cleanedValue = value.replace(/'/g, '"');
+
+      return {
+        ...acc,
+        [key]: cleanedValue,
+      };
+    }, {});
+
+    return newData;
+  });
+};
 
 const toDoubleQuotedJSON = (json) => {
   const JSONString = JSON.stringify(json);
@@ -75,12 +78,25 @@ const toDoubleQuotedJSON = (json) => {
   return JSON.parse(JSONWithDoubleQuotes);
 };
 
+// app starts here
+
+// turn worksheet data into JSON so we can modify the data
+const JSONData = xlsx.utils.sheet_to_json(worksheet);
+
+// perform all the cleansing steps outlined in the Instructions for cleaning Facilities CSV File_061621.docx
+const drupalFormattedJSON = toDoubleQuotedJSON(
+  escapeAposFromRecords(removeDuplicateRecords(restructureRows(removeCOVIDRecords(removeNonApplicable(JSONData))))),
+);
+
 // create workbook
 const newWorkbook = xlsx.utils.book_new();
+
 // Add transformed data to new worksheet
-const newWorksheet = xlsx.utils.json_to_sheet(toDoubleQuotedJSON(escapeAposFromRecords));
+const newWorksheet = xlsx.utils.json_to_sheet(drupalFormattedJSON);
+
 // append worksheet to workbook
 xlsx.utils.book_append_sheet(newWorkbook, newWorksheet, 'Transformed facilities list');
+
 // print that workbook to the project directory
 xlsx.writeFile(newWorkbook, 'transformedFile.xlsx');
 
@@ -93,6 +109,7 @@ const newCSV = xlsx.utils.sheet_to_csv(newWorksheet, {
   blankrows: true,
 });
 
+// write file to file system
 fs.writeFile('transformedCSV.csv', newCSV, (err) => {
   console.error(err);
 });
